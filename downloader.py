cc4191c9b4a8ccc3c25183ea874e43f5a118f914
gongyibei <gongyibei@gmail.com>
2020-06-04 18:35:16 +0800

    add import

--- downloader.py
+++ downloader.py
@@ -1,12 +1,20 @@
+import os
+
+import requests
+from bs4 import BeautifulSoup
+
+
 def mkdir(director):
     if not os.path.exists(director):
         os.mkdir(director)
-        
+
+
 def find_all_pdfurl(html):
     pattern = 'http.+\.pdf'
     pdfurls = re.findall(pattern, html)
     return pdfurls
-    
+
+
 def download_pdfs(year, url, save_dir):
     mkdir(save_dir)
     rsp = requests.get(url)
@@ -24,12 +32,13 @@ def download_pdfs(year, url, save_dir):
                 print('Download failed!')
 
             print(f'Downloaded : {save_dir}/{name}')
-            
+
+
 def get_tasks(year):
     base = 'https://www.music-ir.org'
     url = f'https://www.music-ir.org/mirex/wiki/{year}:MIREX{year}_Results'
     tasks = []
-    
+
     rsp = requests.get(url)
     soup = BeautifulSoup(rsp.text)
     mw = soup.find(class_='mw-parser-output')
@@ -39,20 +48,16 @@ def get_tasks(year):
         for li in ul.children:
             if li.name != 'li':
                 continue
-
             # parse task name
             task_name = li.text.split('\n')[0].rstrip()
             task_name = '_'.join(task_name.split())
-            
             # parse task url
             a = li.find('a')
             task_url = a['href']
             if not task_url.startswith('http'):
                 task_url = f'{base}{task_url}'
-        
-            tasks.append([year, task_name ,task_url])
+            tasks.append([year, task_name, task_url])
     return tasks
-   
 
 
 def download_by_year(year):
@@ -63,6 +68,6 @@ def download_by_year(year):
         mkdir(task_dir)
         download_pdfs(year, url, task_dir)
 
+
 if __name__ == '__main__':
     download_by_year(2019)
-            
